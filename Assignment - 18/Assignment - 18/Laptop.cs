8624562b26709d73c68db93e6ed77b1128a98d86
KKaviya1 <139097458+KKaviya1@users.noreply.github.com>
2023-07-28 15:29:21 +0530

    Add files via upload

---
+++ Assignment - 18/Assignment - 18/Laptop.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment18
+{
+        public class Laptop : IConnectable, IRechargeable, IDisplayable
+        {
+            public bool IsConnected { get; private set; }
+            public int BatteryLevel { get; private set; }
+            public string DisplayInfo { get; private set; }
+
+            public Laptop()
+            {
+                BatteryLevel = 0;
+                DisplayInfo = "Laptop Display";
+            }
+
+            public bool Connect()
+            {
+                 Console.WriteLine("*Connected Successfully - Laptop*");
+                 return true;
+            }
+
+            public void Charge(int minutes)
+            {
+                BatteryLevel += minutes;
+            }
+
+            public string Display()
+            {
+                return DisplayInfo;
+            }
+        }
+
+    
+}
---
+++ Assignment - 18/Assignment - 18/Smartphone.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment18
+{
+    public class Smartphone : IConnectable, IRechargeable, IDisplayable
+    {
+        public bool IsConnected { get; private set; }
+        public int BatteryLevel { get; private set; }
+        public string DisplayInfo { get; private set; }
+
+        public Smartphone()
+        {
+            BatteryLevel = 0;
+            DisplayInfo = "Smartphone Display";
+        }
+
+        public bool Connect()
+        {
+            Console.WriteLine("*Connected Successfully - SmartPhone*");
+            return true;
+        }
+
+        public void Charge(int minutes)
+        {
+            BatteryLevel += minutes;
+        }
+
+        public string Display()
+        {
+            return DisplayInfo;
+        }
+    }
+
+}
